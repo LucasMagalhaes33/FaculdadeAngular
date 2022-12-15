e1d30561c29adda667c4a19685edd36b36f54c76
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-12-15 11:20:01 -0300

    add dialog to courses when ocurred errors to loading datas

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import {  Course  } from "../model/course";
 import {  HttpClient  } from "@angular/common/http";
-import {delay, first, tap} from "rxjs";
+import {  delay, first, tap } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = '/assets/acourses.json';
 
   constructor(private httpClient: HttpClient) { }
 
