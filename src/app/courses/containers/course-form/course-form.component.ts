b9a7287cca2824e84ef3fa884988a77f8ae4c3e7
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-05 12:29:47 -0300

    refatorando components para o padrão SOLID

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {CoursesService} from "../services/courses.service";
+import {FormBuilder, FormControl, NonNullableFormBuilder} from "@angular/forms";
+import {CoursesService} from "../../services/courses.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {Location} from "@angular/common";
 
@@ -11,16 +11,15 @@ import {Location} from "@angular/common";
 })
 export class CourseFormComponent implements OnInit {
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(private formBuilder: NonNullableFormBuilder,
               private service: CoursesService,
               private snackBar: MatSnackBar,
               private location: Location) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
   }
 
   ngOnInit(): void {
